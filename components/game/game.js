6b13c30d4b5f62f31ef6931520afc45486c733f0
Ivan Fedotovsky <fedotovsky96@gmail.com>
2023-08-09 16:23:27 +0400

    add: tailwind.css

--- components/game/game.js
+++ components/game/game.js
@@ -1,7 +1,7 @@
 import { GameCell } from './game-cell';
 import { GameInfo } from './game-info';
-import styles from './game.module.css';
 import { useGameState } from './use-game-state';
+import ResetButton from '../reset-button';
 
 export function Game() {
   const {
@@ -15,13 +15,13 @@ export function Game() {
   } = useGameState();
 
   return (
-    <div className={styles['game']}>
+    <div className="flex flex-col items-center w-40 mx-auto my-24 border border-black p-5">
       <GameInfo
         isDraw={isDraw}
         winnerSymbol={winnerSymbol}
         currentStep={currentStep}
       />
-      <div className={styles['game-field']}>
+      <div className="grid pt-px pl-px grid-cols-[repeat(3,_30px)] grid-rows-[repeat(3,_30px)]">
         {cells.map((symbol, index) => (
           <GameCell
             key={index}
@@ -31,9 +31,7 @@ export function Game() {
           />
         ))}
       </div>
-      <button className={styles['reset']} onClick={resetGame}>
-        Сбросить
-      </button>
+      <ResetButton onClick={resetGame} />
     </div>
   );
 }
---
+++ pages/test.jsx
@@ -0,0 +1,5 @@
+const test = () => {
+  return <div className="text-2xl text-red-800">test</div>;
+};
+
+export default test;
