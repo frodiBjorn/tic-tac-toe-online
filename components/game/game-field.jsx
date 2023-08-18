4a6a74d8ed7b1402d1725ec89f1335ba2372df14
Ivan Fedotovsky <fedotovsky96@gmail.com>
2023-08-18 19:43:14 +0400

    add: useState for game

--- components/game/game-field.jsx
+++ components/game/game-field.jsx
@@ -1,11 +1,46 @@
 import clsx from "clsx";
-import { ZeroIcon } from "./icons/zero-icon";
-import { CrossIcon } from "./icons/cross-icon";
 import { UiButton } from "../uikit/ui-button";
-
-const cells = new Array(19 * 19).fill(null);
+import { GameSymbol } from "./game-symbol";
+import { useGameState } from "./use-game-state";
 
 export const GameField = ({ className }) => {
+  const { cells, currentMove, nextMove, handleCellClick } = useGameState();
+
+  const actions = (
+    <>
+      <UiButton size="md" variant="primary">
+        Ничья
+      </UiButton>
+      <UiButton size="md" variant="outline">
+        Ничья
+      </UiButton>
+    </>
+  );
+
+  return (
+    <GameFieldLayout className={className}>
+      <GameMoveInfo
+        actions={actions}
+        currentMove={currentMove}
+        nextMove={nextMove}
+      />
+      <GameGrid>
+        {cells.map((symbol, index) => (
+          <GameCell
+            key={index}
+            onClick={() => {
+              handleCellClick(index);
+            }}
+          >
+            {symbol && <GameSymbol symbol={symbol} className="w-5 h-5" />}
+          </GameCell>
+        ))}
+      </GameGrid>
+    </GameFieldLayout>
+  );
+};
+
+const GameFieldLayout = ({ children, className }) => {
   return (
     <div
       className={clsx(
@@ -13,32 +48,42 @@ export const GameField = ({ className }) => {
         "bg-white rounded-2xl shadow-md px-6 pt-5 pb-7"
       )}
     >
-      <div className="flex gap-3 items-center">
-        <div className="mr-auto">
-          <div className="flex items-center gap-1 text-xl font-semibold leading-tight">
-            Ход: <ZeroIcon className="w-5 h-5" />
-          </div>
-          <div className="flex items-center gap-1 text-xs text-slate-400 ">
-            Следующий: <CrossIcon />
-          </div>
-        </div>
-        <UiButton size="md" variant="primary">
-          Ничья
-        </UiButton>
-        <UiButton size="md" variant="outline">
-          Сдаться
-        </UiButton>
-      </div>
-      <div className="grid grid-cols-[repeat(19,_30px)] grid-rows-[repeat(19,_30px)] pt-px mt-3">
-        {cells.map((_, cell) => (
-          <button
-            key={cell}
-            className="border border-slate-200 -ml-px -mt-px flex items-center justify-center"
-          >
-            <ZeroIcon className="w-5 h-5" />
-          </button>
-        ))}
-      </div>
+      {children}
     </div>
+  );
+};
+
+const GameMoveInfo = ({ actions, currentMove, nextMove }) => {
+  return (
+    <div className="flex gap-3 items-center">
+      <div className="mr-auto">
+        <div className="flex items-center gap-1 text-xl font-semibold leading-tight">
+          Ход: <GameSymbol symbol={currentMove} className="w-5 h-5" />
+        </div>
+        <div className="flex items-center gap-1 text-xs text-slate-400 ">
+          Следующий: <GameSymbol symbol={nextMove} className="w-3 h-3" />
+        </div>
+      </div>
+      {actions}
+    </div>
+  );
+};
+
+const GameGrid = ({ children }) => {
+  return (
+    <div className="grid grid-cols-[repeat(19,_30px)] grid-rows-[repeat(19,_30px)] pt-px mt-3">
+      {children}
+    </div>
+  );
+};
+
+const GameCell = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="border border-slate-200 -ml-px -mt-px flex items-center justify-center"
+    >
+      {children}
+    </button>
   );
 };
---
+++ components/game/use-game-state.js
@@ -0,0 +1,38 @@
+import { GAME_SYMBOLS, MOVE_ORDER } from "./constants";
+import { useState } from "react";
+
+function getNextMove(currentMove) {
+  const nextMoveIndex = MOVE_ORDER.indexOf(currentMove) + 1;
+  return MOVE_ORDER[nextMoveIndex] ?? MOVE_ORDER[0];
+}
+
+export const useGameState = () => {
+  const [{ cells, currentMove }, setGameState] = useState(() => ({
+    cells: new Array(19 * 19).fill(null),
+    currentMove: GAME_SYMBOLS.CROSS,
+  }));
+
+  const nextMove = getNextMove(currentMove);
+
+  const handleCellClick = (index) => {
+    setGameState((lastGameState) => {
+      if (lastGameState.cells[index]) {
+        return lastGameState;
+      }
+      return {
+        ...lastGameState,
+        currentMove: getNextMove(lastGameState.currentMove),
+        cells: lastGameState.cells.map((cell, i) =>
+          i == index ? lastGameState.currentMove : cell
+        ),
+      };
+    });
+  };
+
+  return {
+    cells,
+    currentMove,
+    nextMove,
+    handleCellClick,
+  };
+};
